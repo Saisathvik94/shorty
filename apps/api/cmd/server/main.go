810077c6e2cde6a9d965d2cfe8af050184b49b13
Saisathvik94 <saisathvik615@gmail.com>
2026-09-16 20:26:25 +0530

    feat(server): Implement graceful shutdown for API server
    *   Utilize `http.Server` for better control over server lifecycle.
    *   Listen for `SIGINT` and `SIGTERM` OS signals for termination.
    *   Introduce a 5-second timeout for graceful shutdown.
    *   Ensures clean termination of server processes, preventing abrupt exits.

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,9 +18,35 @@ func main() {
 	r.GET("/health", func(c *gin.Context) {
 		c.String(200, "Shorty API")
 	})
-	log.Println("API Running on http://localhost:3000")
 
-	if err := r.Run(":3000"); err != nil {
-		log.Fatal(err)
+	// Graceful Shutdown
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: r,
 	}
+	// A goroutine with no name
+	go func() {
+		log.Println("API Running on: http://localhost:3000")
+
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server Failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // SIGINT (Ctrl + C) and SIGTERM (Docker*/ Kubernetes)
+
+	<-quit // wait
+
+	log.Println("Shutting Down the server....")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Forced to ShutDown: %v", err)
+	}
+
+	log.Println("Server Stopped")
 }
